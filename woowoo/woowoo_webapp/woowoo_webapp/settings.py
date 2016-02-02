efe6ae70f83c83752fe8efa339c6ec48babe3a19
Joel Cormack <joel@robottoilets.co.uk>
2016-02-02 11:04:51 +0000

    readded static url setting

--- woowoo/woowoo_webapp/woowoo_webapp/settings.py
+++ woowoo/woowoo_webapp/woowoo_webapp/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_URL = '/static/'
 STATIC_ROOT = '/home/joelcormack/woowoo_webapp/woowoo/woowoo_webapp/woowoo_webapp/static/'
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR,'static'),
